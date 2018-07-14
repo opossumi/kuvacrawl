ede1035c8a80a6103176e1f8fe1000b96cafb0c9
Erkka Saarela <koodi@erkka.fi>
2018-07-14 04:47:00 +0300

    Remove hardcoded kuvat.fi subdomain and download smaller pictures when original is not available

--- kuvacrawl.py
+++ kuvacrawl.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 """
-Copyright (c) 2017 Erkka Saarela
+Copyright (c) 2017-2018 Erkka Saarela
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -38,11 +38,6 @@ import shutil
 import sys
 import time
 
-BASE_URL = "https://tite.kuvat.fi"
-FOLDER_TREE = BASE_URL+"/?type=getFolderTree"
-FILE_LIST = BASE_URL+"/?type=getFileListJSON"
-DATA_DIR = "data"
-
 def RateLimited(limit=None):
     def decorate(func):
         lastTimeCalled = [0.0]
@@ -64,37 +59,22 @@ def RateLimited(limit=None):
         return rateLimitedFunction
     return decorate
 
-def moglify(fname):
-    if fname[0] == '/':
-        fname = fname[1:]
-    return join(DATA_DIR, fname)
-
-def make_folder(dname):
-    dname = moglify(dname)
-    if not os.path.exists(dname):
-        os.makedirs(dname)
-
-def save(fname, data):
-    with open(moglify(fname), 'wb') as f:
-        f.write(data)
-
-def exists(fname):
-    return isfile(moglify(fname))
-
-def load_jsonfile(fname):
-    with open(moglify(fname), 'rb') as f:
-        data = f.read().decode()
-        return json.loads(data)
-
 class KuvaCrawler(object):
-    def __init__(self, ratelimit=None):
+    def __init__(self, datadir, site, ratelimit=None):
+        self.site = site
+        self.datadir = os.path.join(datadir, self.site)
+        if not os.path.exists(self.datadir):
+            os.makedirs(self.datadir)
+        self.base_url = "https://%s.kuvat.fi" % (self.site,)
+        self.folder_tree_url = self.base_url+"/?type=getFolderTree"
+        self.file_list_url = self.base_url+"/?type=getFileListJSON"
         if ratelimit:
             self.ratelimit = ratelimit
         self.s = requests.Session()
         self.s.headers.update({
             "User-Agent": "Teidän API on paska"
             })
-        r = self.s.get(BASE_URL+"/kuvat/")
+        r = self.s.get(self.base_url+"/kuvat/")
 
         m = re.search(r'var sid = \'(.+)\';', r.text)
         if not m:
@@ -122,32 +102,36 @@ class KuvaCrawler(object):
     def fetch_picture(self, filepath, fileurl):
         r = self.s.get(fileurl, stream=True)
         if r.status_code == 200:
-            with open(moglify(filepath), 'wb') as f:
+            with open(self.moglify(filepath), 'wb') as f:
                 for chunk in r:
                     f.write(chunk)
+            return True
+        else:
+            print ("Fetch failed, code", r.status_code)
+            print (r.text)
+            return False
     
-    def crawl_picture(self, data):
+    def crawl_picture(self, data, size="full"):
         filepath = data["filepath"]
         jsonpath = filepath+".json"
-        fileurl = BASE_URL+filepath+"/_full.jpg"
+        fileurl = self.base_url+filepath+"?img="+size
     
         # Only download if needed
-        if exists(filepath) and exists(jsonpath) \
-                and load_jsonfile(jsonpath)["hash"] == data["hash"]:
+        if self.exists(filepath) and self.exists(jsonpath) \
+                and self.load_jsonfile(jsonpath)["hash"] == data["hash"]:
             return False
     
         print ("Fetching pic", filepath)
-        save(filepath+".json", json.dumps(data).encode('utf-8'))
-        self.fetch_picture(filepath, fileurl)
-        return True
-    
+        self.save(filepath+".json", json.dumps(data).encode('utf-8'))
+        return self.fetch_picture(filepath, fileurl)
+
     @RateLimited()
     def crawl_folder(self, folder, fdata):
         print ("Crawling", folder)
 
         # DO NOT HIRE these clowns who made this API
         # POST should be used for posting form data, not for API query parameters
-        r = self.s.post(FILE_LIST, data={"ajaxresponse": "1", "folder": folder})
+        r = self.s.post(self.file_list_url, data={"ajaxresponse": "1", "folder": folder})
         data = r.json()
         if (data["status"] == 0):
             print("Got error from remote api: %s" % data["message"])
@@ -155,10 +139,14 @@ class KuvaCrawler(object):
     
         images = []
         for message in data["message"]:
-            self.crawl_picture(message)
-            images.append(message["filepath"])
+            size = "full"
+            if (fdata["nodown"]):
+                # Fallback when downloading original images is not allowed
+                size = message['url']['sizes'][-1:][0]
+            if self.crawl_picture(message, size=size):
+                images.append(message["filepath"])
     
-        dname = moglify(folder)
+        dname = self.moglify(folder)
         files = [f for f in listdir(dname) if isfile(join(dname, f))]
     
         for img in images:
@@ -176,12 +164,12 @@ class KuvaCrawler(object):
     def authenticate_folder(self, folder, fdata):
         if "KUVATFI_PASSWORD" in os.environ:
             print ("Authenticating folder", folder)
-            r = self.s.get(BASE_URL+"/?q=folderpassword&page=&id=%s&folderpassword=%s" % (fdata["id"], os.environ["KUVATFI_PASSWORD"]))
+            r = self.s.get(self.base_url+"/?q=folderpassword&page=&id=%s&folderpassword=%s" % (fdata["id"], os.environ["KUVATFI_PASSWORD"]))
 
     def crawl(self):
         # Pass user session data to get a 'auth_local_session' cookie
         # Folder password authentication doesn't work without this cookie
-        r = self.s.post(FOLDER_TREE, data={'usersession': self.csid})
+        r = self.s.post(self.folder_tree_url, data={'usersession': self.csid})
         rawdata = r.text
         data = json.loads(rawdata)
 
@@ -190,11 +178,11 @@ class KuvaCrawler(object):
                 self.authenticate_folder(folder, data[folder])
 
         try:
-            olddata = load_jsonfile("FolderTree.json")
+            olddata = self.load_jsonfile("FolderTree.json")
         except Exception as e:
             olddata = {}
-        r = self.s.get(FOLDER_TREE)
-        save("FolderTree.json", r.text.encode('utf-8'))
+        r = self.s.get(self.folder_tree_url)
+        self.save("FolderTree.json", r.text.encode('utf-8'))
         data = json.loads(r.text)
 
         # Handle folder renames
@@ -218,18 +206,18 @@ class KuvaCrawler(object):
 
             if head0 == head1 and tail0 != tail1:
                 print ("Renaming", dname0, "to", dname1)
-                os.rename(moglify(dname0), moglify(dname1))
+                os.rename(self.moglify(dname0), self.moglify(dname1))
 
         f = []
-        for (dirpath, dirnames, filenames) in walk("data"):
+        for (dirpath, dirnames, filenames) in walk(self.datadir):
             for dname in dirnames:
                 fullname = join(dirpath, dname)
                 f.append(fullname)
         
         for folder in data:
-            make_folder(folder)
+            self.make_folder(folder)
             self.crawl_folder(folder, data[folder])
-            dname = moglify(folder[:-1])
+            dname = self.moglify(folder[:-1])
             if dname in f:
                 f.remove(dname)
     
@@ -237,20 +225,42 @@ class KuvaCrawler(object):
             shutil.rmtree(dname)
             print ("Removed", dname)
 
+    def moglify(self, fname):
+        if fname[0] == '/':
+            fname = fname[1:]
+        return join(self.datadir, fname)
+
+    def make_folder(self, dname):
+        dname = self.moglify(dname)
+        if not os.path.exists(dname):
+            os.makedirs(dname)
+
+    def save(self, fname, data):
+        with open(self.moglify(fname), 'wb') as f:
+            f.write(data)
+
+    def exists(self, fname):
+        return isfile(self.moglify(fname))
+
+    def load_jsonfile(self, fname):
+        with open(self.moglify(fname), 'rb') as f:
+            data = f.read().decode()
+            return json.loads(data)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Downloads pictures from a kuvat.fi gallery.')
-    parser.add_argument('--path', nargs=1, type=str, help='Data path')
+    parser.add_argument('--path', default='data', type=str, help='Data path')
     parser.add_argument('--ratelimit', type=float, help='Maximum requests per second')
+    parser.add_argument('--site', default='demo', type=str, help='https://[SITE].kuvat.fi')
     args = parser.parse_args()
 
-    if args.path:
-        DATA_DIR = args.path[0]
-
-    if not isdir(DATA_DIR):
-        sys.stderr.write("ERROR: Data dir %s not found.\n" % (DATA_DIR))
+    if not isdir(args.path):
+        sys.stderr.write("ERROR: Data dir %s not found.\n" % (args.path))
         sys.exit(1)
 
     if not "KUVATFI_PASSWORD" in os.environ:
         print ("WARNING! Pasword not supplied, protected folders won't be downloaded.")
-    crawler = KuvaCrawler(ratelimit=args.ratelimit)
+    crawler = KuvaCrawler(datadir=args.path,
+            site=args.site,
+            ratelimit=args.ratelimit)
     crawler.crawl()
